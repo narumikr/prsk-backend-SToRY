3559e6ebf1a3f0eecf2b49b9555bd904369092a6
Narumikr <narunarumikumiku396@gmail.com>
2026-02-18 00:47:29 +0900

    fix review

--- src/main/java/com/example/untitled/UntitledApplication.java
+++ src/main/java/com/example/untitled/UntitledApplication.java
@@ -1,22 +1,15 @@
 package com.example.untitled;
 
-import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.TimeZone;
 
 @SpringBootApplication
 public class UntitledApplication {
 
-	@PostConstruct
-	public void init() {
-		// Set default timezone to UTC for the entire application
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-	}
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(UntitledApplication.class, args);
 	}
-
 }
